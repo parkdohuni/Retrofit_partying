cf5e95c398440807eda85a1a70c17ceb5b6ebadc
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-02-04 22:43:09 +0900

    Feat: Retrofit Fragment

--- app/src/main/java/com/appdong/retrofitpartying/FirstFragment.kt
+++ app/src/main/java/com/appdong/retrofitpartying/FirstFragment.kt
@@ -1,11 +1,14 @@
 package com.appdong.retrofitpartying
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.inflate
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.appdong.retrofitpartying.databinding.FirstFragmentBinding
@@ -14,6 +17,7 @@ class FirstFragment: Fragment() {
 
     private lateinit var binding: FirstFragmentBinding
     private lateinit var navController: NavController
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -29,6 +33,19 @@ class FirstFragment: Fragment() {
 
         binding.getData.setOnClickListener {
 
+            val repository = Repository()
+            val viewModelFactory = MainViewModelFactory(repository)
+            viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+            viewModel.getPost()
+            viewModel.myResponse.observe(viewLifecycleOwner, Observer {
+                if (it.isSuccessful){
+                    Log.d("Response", it.body()?.categoryName.toString()+"!!!!!!!!!!!!!!!!!!!!!!")
+                    binding.categoryName.text = it.body()?.categoryName.toString()
+                }
+                else{
+                    Log.d("Fail","Fail!!!!!")
+                }
+            })
         }
     }
 }
--- app/src/main/java/com/appdong/retrofitpartying/MainActivity.kt
+++ app/src/main/java/com/appdong/retrofitpartying/MainActivity.kt
@@ -1,40 +1,62 @@
 package com.appdong.retrofitpartying
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.appdong.retrofitpartying.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        with(newConfig) {
+            println(orientation)
+            println(screenLayout)
+        }
+    }
+    private val onDestinationChangedListener =
+        NavController.OnDestinationChangedListener { _, destination, _ ->
+            currFragment = destination.id
+
+        }
     /* View */
+    private var currFragment = R.id.firstFragment
+
     private lateinit var navController: NavController
     lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
     private lateinit var name: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+9
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        navController.addOnDestinationChangedListener(onDestinationChangedListener)
 
-        val repository = Repository()
-        val viewModelFactory = MainViewModelFactory(repository)
-        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
-        viewModel.getPost()
-        viewModel.myResponse.observe(this, Observer {
-            if (it.isSuccessful){
-                Log.d("Response", it.body()?.categoryName.toString()+"!!!!!!!!!!!!!!!!!!!!!!")
-                binding.categoryName.text = it.body()?.categoryName.toString()
-            }
-            else{
-                Log.d("Fail","Fail!!!!!")
-            }
-        })
+//        val repository = Repository()
+//        val viewModelFactory = MainViewModelFactory(repository)
+//        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+//        viewModel.getPost()
+//        viewModel.myResponse.observe(this, Observer {
+//            if (it.isSuccessful){
+//                Log.d("Response", it.body()?.categoryName.toString()+"!!!!!!!!!!!!!!!!!!!!!!")
+//                binding.categoryName.text = it.body()?.categoryName.toString()
+//            }
+//            else{
+//                Log.d("Fail","Fail!!!!!")
+//            }
+//        })
     }
 }
